e4ea903cb3642a80ef9611cd836a0315a22aff41
Witek <witold.zzak@gmail.com>
2025-08-07 16:27:22 +0200

    Added basic features

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import pandas as pd
 import streamlit as st
-st.write("Version 0.1")
-file = st.file_uploader("Upload a CSV")
-file = pd.read_json(file)
-st.dataframe(file)
+import datetime
+
+with open('watch-history.json') as f:
+    file = pd.read_json(f)
+
+def categorize(row):
+    if row['category'] == 'video' and not row['shorts']:
+        return 'movie'
+    elif row['category'] == 'video' and row['shorts']:
+        return 'short'
+    else:
+        return 'ad'
+
+file = file[file['time'].str[:4] == '2024']
+
+file = file.drop(['header','details','description','products','subtitles'],axis='columns')
+
+file['category'] = file['activityControls'].apply(len)
+file['category'] = file['category'].map({1: 'video', 2: 'ads', 3: 'ads'})
+file['shorts'] = file['title'].str.contains('#shorts')
+
+file['type'] = file.apply(categorize, axis=1)
+
+movies = file[file['type'] == 'movie']
+movies['months'] = file['time'].str[5:7]
+movies['hours'] = file['time'].str[11:13]
+movies['date'] = file['time'].str[5:10]
+
+counter = file['type'].value_counts()
+
+monthwatch = movies.groupby('months').size().to_frame(name='movie_count')
+hourwatch = movies.groupby('hours').size().to_frame(name='per_day') / 365
+
+days = movies['date'].value_counts()
+st.dataframe(days)
+
+st.dataframe(movies)
+st.dataframe(counter)
+st.bar_chart(monthwatch)
+st.bar_chart(hourwatch, horizontal=True)
